e65c493fe4e19ce0e64767ec6606a99d90e14c4e
John Flickinger <john.flickinger@ricoh-usa.com>
2022-09-29 13:41:20 -0500

    refactor: adding any type to resolve children type error in primary layout

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -3,7 +3,7 @@ import styles from './PrimaryLayout.module.css';
 
 export interface IPrimaryLayout {}
 
-const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }: any) => {
   return (
     <>
       <Head>
