bfcb8c948acce8dfc94837657857253047f90ab3
John Flickinger <john.flickinger@ricoh-usa.com>
2022-10-04 10:03:03 -0500

    feat: added search result component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,31 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import Search from '../components/utility/search/Search';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
+  const { locale } = useRouter();
   return (
-    <section>
-      <div className={'flex flex-col items-center pb-3'}>
-        <Image
-          src="/Google.png"
-          alt="Google Logo"
-          width={272}
-          height={92}
-          priority
-        />
-      </div>
+    <section className={'flex flex-col items-center gap-y-5 mt-12 sm:mt-36'}>
+      <Image
+        src="/Google.png"
+        alt="Google Logo"
+        width={272}
+        height={92}
+        priority
+      />
       <Search />
-      language toggle
+      {/*
+        This would be how to add locale switching
+      */}
+      <p>
+        Google offered in:{' '}
+        <Link href="/" locale={locale === 'en' ? 'fr' : 'en'}>
+          <a className="underline text-blue-600">Français</a>
+        </Link>
+      </p>
     </section>
   );
 };
